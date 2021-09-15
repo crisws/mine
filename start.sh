e0e2826fd3627437d395c3cad111d5bd0e5c6d84
Ubuntu <crisws@Mi.q41yhnch3ckubkuzzvpnc524uc.xx.internal.cloudapp.net>
2021-09-15 04:14:38 +0000

    respaldo

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+screen -d -m -S "mc_spigot_server" ./startserver.sh
---
+++ startserver.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+JAR=craftbukkit-1.17.1.jar
+MAXRAM=2048M
+MINRAM=2048M
+TIME=20
+
+while [ true ]; do
+	    java -Xmx$MAXRAM -Xms$MINRAM -jar $JAR nogui
+	        if [[ ! -d "exit_codes" ]]; then
+			        mkdir "exit_codes";
+    fi
+        if [[ ! -f "exit_codes/server_exit_codes.log" ]]; then
+		        touch "exit_codes/server_exit_codes.log";
+			    fi
+ echo "[$(date +"%d.%m.%Y %T")] ExitCode: $?" >> exit_codes/server_exit_codes.log
+     echo "----- Press enter to prevent the server from restarting in $TIME seconds -----";
+     read -t $TIME input;
+         if [ $? == 0 ]; then
+		         break;
+		 else
+			         echo "------------------- SERVER RESTARTS -------------------";
+				     fi
+			     done
+
